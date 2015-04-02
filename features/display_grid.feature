df65d166fad82fed5821cd2018f181abb0fd2f86
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-04-02 14:32:11 +0300

    Can see 5x5 grid now

--- features/display_grid.feature
+++ features/display_grid.feature
@@ -6,3 +6,10 @@ Feature:  Display grid to screen after a shot has been fired
     Scenario: I want to see an empty grid before starting
         Given I am on the Battleships page
         Then I should see the table
+
+	Scenario: I want to see a 5x5 grid before starting
+	    Given I am on the Battleships page
+	    Then I should see the table
+	    And the grid should have 5 rows
+	    And the grid should have 5 columns
+
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -11,5 +11,15 @@ Then(/^I should see 'error page'$/) do
 end
 
 Then(/^I should see the table$/) do
-  page.should have_xpath("//table[@data-purpose = 'board' and @data-user='player_1']")
+  page.should have_xpath("//table[@data-purpose='board' and @data-user='player_1']")
+end
+
+Then(/^the grid should have (\d+) rows$/) do |arg1|
+  expect(page.all("//table[@data-purpose='board'][@data-user='player_1']/tr").count).to eq arg1.to_i
+end
+
+Then(/^the grid should have (\d+) columns$/) do |arg1|
+  page.all("//table[@data-purpose='board'][@data-user='player_1']/tr").each_with_index do |el, index| 
+    expect(page.all("//table[@data-purpose='board'][@data-user='player_1']/tr[@data-row='#{index+1}']/td").count).to eq arg1.to_i
+  end
 end
