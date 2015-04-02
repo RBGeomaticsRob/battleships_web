6066265f265ec9c2d91418915f3ac7ae58703559
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-02 11:57:44 +0100

    Added tests and passed to add a empty grid to battleships page

---
+++ features/display_grid.feature
@@ -0,0 +1,8 @@
+Feature:  Display grid to screen after a shot has been fired
+          In order to see current situation
+          As a player
+          I want to see the grid
+
+    Scenario: I want to see an empty grid before starting
+        Given I am on the Battleships page
+        Then I should see the table
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -6,23 +6,10 @@ Then(/^I should see "([^"]*)"$/) do |arg1|
   expect(page).to have_content(arg1)
 end
 
-Given(/^I already have ship in "([^"]*)"$/) do |arg1|
-  fill_in('location', :with => arg1)
-  fill_in('direction', :with => :south)
-  click_button('Submit')
-end
-
 Then(/^I should see 'error page'$/) do
   expect(page).to have_content('There\'s been a collision!')
 end
 
-When(/^I send a hit to "([^"]*)"$/) do |arg1|
-  fill_in('coordinate', :with => arg1)
-  click_button('submit')
-end
-
-When(/^I place ship in "([^"]*)"$/) do |arg1|
-  fill_in('location', :with => arg1)
-  fill_in('direction', :with => :south)
-  click_button('Submit')
+Then(/^I should see the table$/) do
+  page.should have_xpath("//table[@data-purpose = 'board' and @data-user='player_1']")
 end
